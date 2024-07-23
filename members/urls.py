5ab9cb001d8185fbdd29a219ff607e04088d5998
Mikheil Kuprava <mishiko.kuprava20@gmail.com>
2024-07-24 00:32:53 +0400

    test2

--- members/urls.py
+++ members/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.members, name='members'),
     path('details/<int:id>', views.details, name='details'),
+    path('add', views.add, name='add'),
 ]
--- members/views.py
+++ members/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
+from django.middleware.csrf import get_token
 
 def members(request):
+  c = {'csrf_token': get_token(request)} 
   mymembers = Member.objects.all().values()
   template = loader.get_template('all_members.html')
   context = {
     'mymembers': mymembers,
+    'token': c
   }
   return HttpResponse(template.render(context, request))
 
@@ -17,3 +20,19 @@ def details(request, id):
     'mymember': mymember,
   }
   return HttpResponse(template.render(context, request))
+
+def add(request):
+    if request.method == "POST":
+        user_data = Member(
+            firstname=request.POST.get('name'),
+            lastname=request.POST.get('lastname'),
+            age=request.POST.get('age')
+        )
+        user_data.save()
+
+    members = Member.objects.all()  # Fetch all members to pass to the template
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers': members
+    }
+    return HttpResponse(template.render(context, request))
